ff17b2ccb9dda76b740bb581ab04256a0c97bf0e
Bruno Melo <bmc4@cin.ufpe.br>
2025-05-27 08:46:31 -0300

    A few changes

--- src/module/data/SR5ItemDataWrapper.ts
+++ src/module/data/SR5ItemDataWrapper.ts
@@ -436,6 +436,7 @@ export class SR5ItemDataWrapper extends DataWrapper<ShadowrunItemData> {
     getTechnology(): TechnologyData|undefined {
         if ("technology" in this.data.system)
             return this.data.system.technology;
+        return;
     }
 
     getRange(): CritterPowerRange|SpellRange|RangeWeaponData|undefined {
@@ -449,6 +450,7 @@ export class SR5ItemDataWrapper extends DataWrapper<ShadowrunItemData> {
 
         if (this.data.type === 'weapon')
             return this.data.system.range as RangeWeaponData;
+        return;
     }
 
     getModificationCategory(): string {
